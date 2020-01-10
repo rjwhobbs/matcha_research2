c8b9cd10ebe698d4373ca7b61e4d5c029508a69f
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-01-10 14:29:14 +0200

    Removed commneted code

--- auth/passport_init.js
+++ auth/passport_init.js
@@ -4,7 +4,6 @@ const conn 			= require('../connection/conn');
 const _ 			= require('lodash');
 const bcrypt		= require('bcrypt');
 let	sql				= require('../sql/statements');	
-// let users 			= require('../dummy_data/users.json');
 
 passport.use(new localStrategy(function(username, password, done) {
 	conn.query(sql.selUser, [username], (err, results, feilds) => {
@@ -18,12 +17,6 @@ passport.use(new localStrategy(function(username, password, done) {
 				done(null, false);
 			}
 		});
-		// let user = _.find(users, u => u.name === username);
-		// if (!user || user.password !== password) {
-		// 	done(null, false); // Null = no error, false = auth failed, no user
-		// 	return ;
-		// }
-		// done(null, user);
 	});
 }));
 
@@ -32,11 +25,9 @@ passport.serializeUser(function(user, done) {
 });
 // Adds the user id to the session var
 passport.deserializeUser(function(user_id, done) {
-	// let user = _.find(users, u => u.id === id);
 	conn.query(sql.selUserById, [user_id], (err, results) => {
 		if (err) {throw err}
 		let user = results[0];
 		done(null, user);
 	});
-	// done(null, user);
 });
